1e0e3a7c5840fdc51dcf9049ed3541f806204668
faom002 <faisal_jobb@outlook.com>
2024-05-22 01:47:29 -0700

    added to gitinaore

--- obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76db94b5f84d87e3b37fdf199170454f140a9d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93904a7f90adb02808b8e54c93715693d67a9bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
